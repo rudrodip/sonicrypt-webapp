4fc6d6dbad85d2e5a4fe32520074bafac3ed7760
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-08 22:41:19 +0600

    Add marketing page component

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Section from "@/components/section";
+
+export default function Home() {
+  return (
+    <Section className="min-h-screen">
+      <Button>hello</Button>
+    </Section>
+  );
+}
