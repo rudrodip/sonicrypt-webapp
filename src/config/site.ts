05edb58ab746caafdc432e52d99f65b01a9d46e1
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-08 22:46:20 +0600

    Add site configuration file

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Sonicrypt",
+  description:
+    "Sonicrypt is a compact and intuitive tool designed to enhance the user experience for both buyers and sellers engaging in crypto transactions",
+  url: "https://rudrodip.tech",
+  ogImage: "https://rudrodip.tech/og.png",
+  links: {
+    twitter: "https://twitter.com/rds_agi",
+    github: "https://github.com/rudrodip",
+  },
+};
