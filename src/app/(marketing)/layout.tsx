fa19f9e5395b3b049c26d45ab75d2a1fac11f42d
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-11 15:21:15 +0600

    Refactor marketing layout component

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,8 +1,9 @@
-import { Icons } from "@/components/icons";
-import { DefaultNavbar } from "@/components/navbar/default-navbar";
-import ThemeToggler from "@/components/theme/theme-toggler";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Section from "@/components/section";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowRightIcon } from "lucide-react";
+import { DefaultGlyphButton } from "@/components/glyph-button";
+import { cn } from "@/lib/utils";
 
 export default function RootLayout({
   children,
@@ -11,23 +12,40 @@ export default function RootLayout({
 }>) {
   return (
     <section className="flex min-h-screen flex-col">
-      <header className="flex justify-between items-center w-full px-10 sticky top-0 mx-auto border">
-        <Button size="icon" variant="ghost">
-          <Image
-            src="/logo.png"
-            alt="sonicrypt"
-            width={30}
-            height={30}
-            className="w-full h-full"
-          />
-        </Button>
-        <DefaultNavbar />
-        <ThemeToggler />
+      <header className="flex justify-between items-center w-full mx-auto border-b bg-background">
+        <Section className="flex justify-between items-center w-full">
+          <div className="flex">
+            <Link href="/about" className={buttonVariants({ variant: "link" })}>
+              About
+            </Link>
+            <Link href="/docs" className={buttonVariants({ variant: "link" })}>
+              Docs
+            </Link>
+          </div>
+          <Link href="/" className="text-xl">
+            <DefaultGlyphButton
+              text="Sonicrypt"
+              className="text-center flex items-center"
+            />
+          </Link>
+          <div className="flex items-center">
+            <Link
+              href="/signup"
+              className={cn(
+                buttonVariants({ variant: "link", size: "sm" }),
+                "group hover:bg-transparent"
+              )}
+            >
+              <p>Get started</p>
+              <ArrowRightIcon
+                size={18}
+                className="ml-2 opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-200 ease-in-out"
+              />
+            </Link>
+          </div>
+        </Section>
       </header>
       <main className="w-full">{children}</main>
-      <footer>
-        <p></p>
-      </footer>
     </section>
   );
 }
