198b5ff0e66a5e427272169bb63cfe16eff03ea2
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-11 15:23:55 +0600

    Add features configuration file

---
+++ src/config/features.ts
@@ -0,0 +1,17 @@
+export const features = [
+    {
+        id: 1,
+        title: "Auditory Confirmation",
+        description: "Sonicrypt emits a distinct sound upon completion of a cryptocurrency transaction, providing users with immediate confirmation and peace of mind.",
+    },
+    {
+        id: 2,
+        title: "User-Friendly Interface",
+        description: "With its intuitive design, Sonicrypt ensures ease of use for users of all technological backgrounds, facilitating seamless integration into existing payment systems.",
+    },
+    {
+        id: 3,
+        title: "Extensibility",
+        description: "Sonicrypt features generic interfaces, accommodating multiple use cases such as managing transactions across various wallets, supporting diverse currencies.",
+    },
+];
