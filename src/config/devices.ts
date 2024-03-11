f2f97961ed6083ad0a33bde67ea4dd65e5ec5e49
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-11 15:23:50 +0600

    Add devices configuration file

---
+++ src/config/devices.ts
@@ -0,0 +1,37 @@
+export const devices = {
+    micro: {
+        name: "Sonicrypt micro",
+        image: "/logo.png",
+        features: [
+            "Miniature size",
+            "Low power consumption",
+            "Low cost"
+        ],
+        doc: "/docs/micro",
+        status: "done",
+    },
+    mini: {
+        name: "Sonicrypt mini",
+        image: "/logo.png",
+        features: [
+            "Comes with display for dynamic QR code",
+            "Low power consumption",
+            "Connects to smartphone"
+        ],
+        doc: "/docs/mini",
+        status: "in progress",
+    },
+    base: {
+        name: "Sonicrypt",
+        image: "/logo.png",
+        features: [
+            "Dynamic QR code + Dynamic audio",
+            "Custom local blockchain indexer",
+            "High speed",
+            "Unlimited wallet support",
+            "No subscription fees",
+        ],
+        doc: "/docs/sonicrypt",
+        status: "in progress",
+    }
+}
