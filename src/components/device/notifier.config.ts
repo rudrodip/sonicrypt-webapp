c01a902b7d95a47fd52f46a0d83c4aa6ff304d8f
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-28 23:19:19 +0600

    Add notifier configuration file

---
+++ src/components/device/notifier.config.ts
@@ -0,0 +1,26 @@
+export const notifierConfig = [
+    {
+        id: 1,
+        key: "A",
+        backgroundColor: "#f54269",
+        audioPath: "/audio/B.mp3",
+    },
+    {
+        id: 2,
+        key: "S",
+        backgroundColor: "#cbf542",
+        audioPath: "/audio/C.mp3",
+    },
+    {
+        id: 3,
+        key: "D",
+        backgroundColor: "#66d968",
+        audioPath: "/audio/F.mp3",
+    },
+    {
+        id: 4,
+        key: "F",
+        backgroundColor: "#42aaf5",
+        audioPath: "/audio/E.mp3",
+    },
+];
