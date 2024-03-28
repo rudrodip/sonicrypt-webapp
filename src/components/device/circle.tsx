5fe9fe9daad9c544cf6bc0aa397cad907098c944
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-28 23:19:08 +0600

    Add Circle component for device interaction

---
+++ src/components/device/circle.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { motion } from "framer-motion";
+import ReactHowler from "react-howler";
+import { useState } from "react";
+
+export const Circle = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <motion.div
+      id="c"
+      className="parent-c cursor-pointer"
+      drag
+      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+      whileHover={{
+        scale: 1.02,
+        transition: { duration: 0.35 },
+      }}
+      whileTap={{ scale: 0.95 }}
+      whileDrag={{ scale: 1.05, opacity: 0.9 }}
+      onClick={() => setIsPlaying(!isPlaying)}
+    >
+      <ReactHowler
+        src="/audio/click-1.mp3"
+        playing={isPlaying}
+        onEnd={() => setIsPlaying(false)}
+      />
+      <motion.div
+        className="child-c"
+        animate={{
+          backgroundColor: ["#807F84", "#BAF2BB"],
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "mirror",
+        }}
+      ></motion.div>
+    </motion.div>
+  );
+};
