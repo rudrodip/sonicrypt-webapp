a913cffe53d34594c48c2d4783f2c78a075595e3
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-28 23:18:26 +0600

    Add WalletButton component to the marketing layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,9 +1,9 @@
-import Section from "@/components/section";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRightIcon } from "lucide-react";
 import { DefaultGlyphButton } from "@/components/glyph-button";
 import { cn } from "@/lib/utils";
+import WalletButton from "@/components/wallet-button";
 
 export default function RootLayout({
   children,
@@ -11,39 +11,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="flex min-h-screen flex-col">
-      <header className="flex justify-between items-center w-full mx-auto border-b bg-background">
-        <Section className="flex justify-between items-center w-full">
-          <div className="flex">
-            <Link href="/about" className={buttonVariants({ variant: "link" })}>
-              About
-            </Link>
-            <Link href="/docs" className={buttonVariants({ variant: "link" })}>
-              Docs
-            </Link>
-          </div>
-          <Link href="/" className="text-xl">
-            <DefaultGlyphButton
-              text="Sonicrypt"
-              className="text-center flex items-center"
-            />
+    <section className="flex min-h-screen flex-col overflow-x-hidden">
+      <header className="flex justify-between items-center w-full mx-auto border-b bg-background z-[5000]">
+        <div className="flex mx-10">
+          <Link href="/about" className={buttonVariants({ variant: "link" })}>
+            About
           </Link>
-          <div className="flex items-center">
-            <Link
-              href="/signup"
-              className={cn(
-                buttonVariants({ variant: "link", size: "sm" }),
-                "group hover:bg-transparent"
-              )}
-            >
-              <p>Get started</p>
-              <ArrowRightIcon
-                size={18}
-                className="ml-2 opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-200 ease-in-out"
-              />
-            </Link>
-          </div>
-        </Section>
+          <Link href="/docs" className={buttonVariants({ variant: "link" })}>
+            Docs
+          </Link>
+        </div>
+        <Link href="/" className="text-xl">
+          <DefaultGlyphButton
+            text="Sonicrypt"
+            className="text-center flex items-center font-heading lowercase"
+          />
+        </Link>
+        <div className="flex items-center">
+          <WalletButton />
+        </div>
       </header>
       <main className="w-full">{children}</main>
     </section>
