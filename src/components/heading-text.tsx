2066ec7c032d07eeec1265e0a4b4fd02ebdec833
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-28 23:18:51 +0600

    Add HeadingText component with animation

---
+++ src/components/heading-text.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { motion, stagger } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    x: 100,
+    y: 40,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+export const HeadingText = () => {
+  return (
+    <motion.div
+      variants={variants}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true }}
+      className="overflow-hidden"
+    >
+      <motion.h1 variants={item} id="first-line" className="head-text">
+        Building <motion.span className="highlighted">sound</motion.span>
+      </motion.h1>
+      <motion.h1 variants={item} id="second-line" className="head-text">
+        & <motion.span className="highlighted">sight</motion.span> to silent
+      </motion.h1>
+      <motion.h1 variants={item} id="third-line" className="head-text">
+        transactions
+      </motion.h1>
+    </motion.div>
+  );
+};
