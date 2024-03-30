062f066602773ea99270fa2f943047cbc9a9c0bc
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-30 23:46:33 +0600

    Refactor Device component to accept props and add new functionality

--- src/components/device/index.tsx
+++ src/components/device/index.tsx
@@ -6,43 +6,75 @@ import { notifierConfig } from "./notifier.config";
 import { Circle } from "./circle";
 import { Module } from "./module";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { cn } from "@/lib/utils";
 
-export default function Device() {
+interface DeviceProps {
+  orientation: "horizontal" | "vertical";
+  listenToKeyboard: boolean;
+  listenToTx: boolean;
+  triggerPlaying: boolean;
+}
+
+export default function Device({
+  orientation,
+  listenToKeyboard,
+  listenToTx,
+  triggerPlaying,
+}: DeviceProps) {
   const maxWidth = useMediaQuery("(max-width: 774px)");
 
   return (
     <motion.div
       id="body"
-      initial={{ y: 50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1, rotate: maxWidth ? -90 : 0 }}
+      initial={{
+        y: 50,
+        opacity: 0,
+        rotate: orientation === "vertical" ? -90 : 0,
+      }}
+      animate={{
+        y: 0,
+        opacity: 1,
+        rotate: orientation === "vertical" ? -90 : 0,
+      }}
       transition={{ duration: 1 }}
-      className="device-body"
+      className="relative rounded-md w-[768px] mx-auto aspect-[2/1] bg-[#ECECEC] pebble-shadow z-10"
     >
-      <motion.div className="elements-container">
-        <motion.div className="notifier-box">
+      <motion.div className="h-full w-[576px] mx-auto flex justify-between items-center">
+        <motion.div className="relative h-full w-14 border border-[#ECECEC] flex flex-col justify-between bg-[#DEDEDE] pebble-shadow">
           {notifierConfig.map((notifier, idx) => (
             <Notifier
               key={notifier.id}
               keyPress={notifier.key}
               backgroundColor={notifier.backgroundColor}
               audioPath={notifier.audioPath}
+              listenToKeyboard={listenToKeyboard}
             />
           ))}
-          <motion.div id="red-button" className="red-button"></motion.div>
+          <motion.div
+            id="red-button"
+            className="bg-[#e55a6b] absolute w-full h-1 left-0 -bottom-1 rounded-b cursor-pointer"
+          ></motion.div>
         </motion.div>
-        <motion.div className="module-container">
-          <Module>
+        <motion.div className="relative h-full w-52 flex justify-center items-center">
+          <Module
+            triggerPlaying={triggerPlaying}
+            listenToTx={listenToTx}
+            listenToKeyboard={listenToKeyboard}
+          >
             <Circle />
           </Module>
-          <motion.div id="button-container" className="button-container">
+          <motion.div
+            id="button-container"
+            className="absolute w-full h-1 left-0 -bottom-1 rounded-b flex justify-end gap-5"
+          >
             <motion.div
               id="btn-1"
-              className="device-button"
+              className="w-14 h-full bg-[#807F84] cursor-pointer"
               whileTap={{ y: -1 }}
             ></motion.div>
             <motion.div
               id="btn-2"
-              className="device-button"
+              className="w-14 h-full bg-[#807F84] cursor-pointer"
               whileTap={{ y: -1 }}
             ></motion.div>
           </motion.div>
