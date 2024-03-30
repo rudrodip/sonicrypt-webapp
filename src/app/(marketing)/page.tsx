15dfb7398a2bf0914d56418c96981e6541eb47a0
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-30 23:46:26 +0600

    Add Features component to Home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { ClientOnly } from "@/components/client";
+import Features from "@/components/features";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <ClientOnly>
       <Hero />
+      <Features />
     </ClientOnly>
   );
 }
