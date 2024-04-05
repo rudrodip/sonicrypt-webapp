6aef8d37161a493077f253ba3fa1baedc5a1c217
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-05 07:31:36 +0600

    Remove unused component import

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { ClientOnly } from "@/components/client";
-import Features from "@/components/features";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <ClientOnly>
       <Hero />
-      <Features />
     </ClientOnly>
   );
 }
