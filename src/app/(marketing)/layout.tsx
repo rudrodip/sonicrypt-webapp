592ae7b68ef5bf7d5704681e64a4b0f2ac8321f9
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-05 07:31:29 +0600

    Update marketing layout header styling

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -10,11 +10,11 @@ export default function RootLayout({
 }>) {
   return (
     <section className="flex min-h-screen flex-col">
-      <header className="flex justify-between items-center w-full border-b bg-background z-[5000] overflow-hidden">
-        <div className="flex pl-20 items-center">
+      <header className="flex justify-between items-center w-full border-b bg-background px-20 z-[5000] overflow-hidden">
+        <div className="flex items-center">
           <DefaultGlyphButton
             text="Sonicrypt"
-            className="text-center flex items-center lowercase pl-0"
+            className="text-center flex items-center lowercase pl-0 font-link text-xl"
           />
           <Link href="/about" className={buttonVariants({ variant: "link" })}>
             About
