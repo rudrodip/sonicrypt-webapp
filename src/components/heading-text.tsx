6d9c08f236285cd9447f37b492871f721da30055
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-05 16:51:43 +0600

    Refactor heading-text component to update layout and add motion effects

--- src/components/heading-text.tsx
+++ src/components/heading-text.tsx
@@ -1,15 +1,44 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export const HeadingText = () => {
   return (
-    <div className="overflow-hidden mx-20 head-text">
-      <h1 id="first-line">
-        Building <span className="highlighted">sound</span>
-      </h1>
-      <h1 id="second-line">
-        & <span className="highlighted">sight</span> to silent
-      </h1>
-      <h1 id="third-line">
-        transactions
-      </h1>
-    </div>
+    <>
+      <motion.h1 className="text-center head-text tracking-tight">
+        SONICRYPT
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, translateY: 20 }}
+        animate={{ opacity: 1, translateY: 0, transition: { delay: 0.7 } }}
+        className="text-center max-w-xl mx-auto font-sans font-semibold text-4xl md:text-5xl tracking-tight"
+      >
+        Bring{" "}
+        <motion.span
+          animate={{ backgroundColor: ["#ef4444", "#db2777"] }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "mirror",
+          }}
+          className="bg-clip-text text-transparent"
+        >
+          sound
+        </motion.span>{" "}
+        and{" "}
+        <motion.span
+          animate={{ backgroundColor: ["#db2777", "#ef4444"] }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "mirror",
+          }}
+          className="bg-clip-text text-transparent"
+        >
+          sight
+        </motion.span>{" "}
+        to your <span className="gray">silent transactions</span>
+      </motion.p>
+    </>
   );
 };
