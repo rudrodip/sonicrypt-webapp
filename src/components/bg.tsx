a798eba3daf3d45e8a339bc5caac767cc80f7758
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-05 16:51:40 +0600

    Refactor (marketing) page component, update layout, and remove unused features

---
+++ src/components/bg.tsx
@@ -0,0 +1,441 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function BG() {
+  return (
+    <motion.svg
+      width="1440"
+      height="900"
+      viewBox="0 0 1440 900"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="main"
+      initial="initial"
+      animate="animate"
+    >
+      <motion.g id="bg" clip-path="url(#clip0_82_15)">
+        <motion.g id="vertical-lines">
+          <motion.line
+            id="Line 2"
+            y1="89.5"
+            x2="1440"
+            y2="89.5"
+            stroke="url(#paint0_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 3"
+            y1="177.5"
+            x2="1440"
+            y2="177.5"
+            stroke="url(#paint1_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 4"
+            y1="265.5"
+            x2="1440"
+            y2="265.5"
+            stroke="url(#paint2_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 5"
+            y1="353.5"
+            x2="1440"
+            y2="353.5"
+            stroke="url(#paint3_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+        </motion.g>
+        <motion.g id="horizontal-lines">
+          <motion.line
+            id="Line 12"
+            x1="449.5"
+            y1="551"
+            x2="449.5"
+            y2="-9"
+            stroke="url(#paint4_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 13"
+            x1="539.5"
+            y1="551"
+            x2="539.5"
+            y2="-9"
+            stroke="url(#paint5_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 14"
+            x1="629.5"
+            y1="551"
+            x2="629.5"
+            y2="-9"
+            stroke="url(#paint6_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 15"
+            x1="719.5"
+            y1="551"
+            x2="719.5"
+            y2="-9"
+            stroke="url(#paint7_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 16"
+            x1="809.5"
+            y1="551"
+            x2="809.5"
+            y2="-9"
+            stroke="url(#paint8_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+          <motion.line
+            id="Line 17"
+            x1="899.5"
+            y1="551"
+            x2="899.5"
+            y2="-9"
+            stroke="url(#paint9_linear_82_15)"
+            variants={{
+              initial: { pathLength: 0 },
+              animate: {
+                pathLength: 1,
+                transition: { duration: 1, ease: "easeInOut" },
+              },
+            }}
+          />
+        </motion.g>
+        <motion.rect
+          id="Rectangle 1"
+          x="450"
+          y="89"
+          width="90"
+          height="88"
+          fill="#EFEFEF"
+          fill-opacity="0.12"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.12,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 8"
+          x="450"
+          y="177"
+          width="90"
+          height="89"
+          fill="#D9D9D9"
+          fill-opacity="0.07"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.07,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 9"
+          x="720"
+          y="178"
+          width="90"
+          height="87"
+          fill="#D9D9D9"
+          fill-opacity="0.15"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.15,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 10"
+          x="900"
+          y="178"
+          width="90"
+          height="87"
+          fill="#D9D9D9"
+          fill-opacity="0.13"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.13,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 11"
+          x="720"
+          y="353"
+          width="90"
+          height="86"
+          fill="#D9D9D9"
+          fill-opacity="0.16"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.16,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 2"
+          x="630"
+          y="178"
+          width="90"
+          height="87"
+          fill="#CACACA"
+          fill-opacity="0.1"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.1,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 3"
+          x="900"
+          y="89"
+          width="90"
+          height="89"
+          fill="#D9D9D9"
+          fill-opacity="0.07"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.07,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 5"
+          x="360"
+          y="265"
+          width="90"
+          height="88"
+          fill="#D9D9D9"
+          fill-opacity="0.06"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.06,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+        <motion.rect
+          id="Rectangle 6"
+          x="810"
+          y="353"
+          width="90"
+          height="86"
+          fill="#D9D9D9"
+          fill-opacity="0.06"
+          variants={{
+            initial: { opacity: 0 },
+            animate: {
+              opacity: 0.06,
+              transition: { duration: 1, ease: "easeInOut" },
+            },
+          }}
+        />
+      </motion.g>
+      <defs>
+        <motion.linearGradient
+          id="paint0_linear_82_15"
+          x1="0"
+          y1="90.5"
+          x2="1440"
+          y2="90.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0" />
+          <stop offset="0.41" stop-color="#C7C7C7" stop-opacity="0.24" />
+          <stop offset="1" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint1_linear_82_15"
+          x1="0"
+          y1="178.5"
+          x2="1440"
+          y2="178.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0" />
+          <stop offset="0.465" stop-color="#999999" stop-opacity="0.41" />
+          <stop offset="0.98" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint2_linear_82_15"
+          x1="0"
+          y1="266.5"
+          x2="1440"
+          y2="266.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0.01" />
+          <stop offset="0.495" stop-color="#999999" stop-opacity="0.17" />
+          <stop offset="0.985" stop-color="#999999" stop-opacity="0.02" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint3_linear_82_15"
+          x1="0"
+          y1="354.5"
+          x2="1440"
+          y2="354.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0" />
+          <stop offset="0.645" stop-color="#999999" stop-opacity="0.2" />
+          <stop offset="0.98" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint4_linear_82_15"
+          x1="450.5"
+          y1="-9"
+          x2="450.5"
+          y2="551"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0.02" />
+          <stop offset="0.285" stop-color="#999999" stop-opacity="0.24" />
+          <stop offset="0.99" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint5_linear_82_15"
+          x1="540.5"
+          y1="-9"
+          x2="540.5"
+          y2="551"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0.02" />
+          <stop offset="0.39" stop-color="#999999" stop-opacity="0.29" />
+          <stop offset="0.975" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint6_linear_82_15"
+          x1="630.5"
+          y1="-9"
+          x2="630.5"
+          y2="551"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0" />
+          <stop offset="0.5" stop-color="#CACACA" stop-opacity="0.192" />
+          <stop offset="1" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint7_linear_82_15"
+          x1="720.5"
+          y1="-9"
+          x2="720.5"
+          y2="551"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0" />
+          <stop offset="0.445" stop-color="#D2D2D2" stop-opacity="0.23" />
+          <stop offset="1" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint8_linear_82_15"
+          x1="810.5"
+          y1="-9"
+          x2="810.5"
+          y2="551"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0.01" />
+          <stop offset="0.715" stop-color="#CACACA" stop-opacity="0.26" />
+          <stop offset="1" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <motion.linearGradient
+          id="paint9_linear_82_15"
+          x1="900.5"
+          y1="-9"
+          x2="900.5"
+          y2="551"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="white" stop-opacity="0" />
+          <stop offset="0.39" stop-color="#999999" stop-opacity="0.43" />
+          <stop offset="0.7" stop-color="#999999" stop-opacity="0.17" />
+          <stop offset="0.98" stop-color="#999999" stop-opacity="0" />
+        </motion.linearGradient>
+        <clipPath id="clip0_82_15">
+          <rect width="1440" height="900" fill="white" />
+        </clipPath>
+      </defs>
+    </motion.svg>
+  );
+}
