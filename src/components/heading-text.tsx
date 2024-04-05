f3620c986433449cd0d1cd7b069e6b9e325c1c8e
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-05 07:31:38 +0600

    Refactor heading-text component

--- src/components/heading-text.tsx
+++ src/components/heading-text.tsx
@@ -1,49 +1,15 @@
-"use client";
-
-import { motion, stagger } from "framer-motion";
-
-const variants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const item = {
-  hidden: {
-    opacity: 0,
-    y: 40,
-  },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.4,
-    },
-  },
-};
-
 export const HeadingText = () => {
   return (
-    <motion.div
-      variants={variants}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true }}
-      className="overflow-hidden mx-20 head-text"
-    >
-      <motion.h1 variants={item} id="first-line">
-        Building <motion.span className="highlighted">sound</motion.span>
-      </motion.h1>
-      <motion.h1 variants={item} id="second-line">
-        & <motion.span className="highlighted">sight</motion.span> to silent
-      </motion.h1>
-      <motion.h1 variants={item} id="third-line">
+    <div className="overflow-hidden mx-20 head-text">
+      <h1 id="first-line">
+        Building <span className="highlighted">sound</span>
+      </h1>
+      <h1 id="second-line">
+        & <span className="highlighted">sight</span> to silent
+      </h1>
+      <h1 id="third-line">
         transactions
-      </motion.h1>
-    </motion.div>
+      </h1>
+    </div>
   );
 };
