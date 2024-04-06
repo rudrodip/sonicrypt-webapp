3b886e8bb3108e4e810918356e0c6722cc0cb975
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-06 16:21:12 +0600

    Add Workflow component to marketing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import Hero from "@/components/hero";
+import MobileAppSection from "@/components/mobile-app";
+import Specs from "@/components/specifications";
 import Workflow from "@/components/workflow";
 
 export default function Home() {
   return (
-    <div className="px-2 md:px-10">
+    <div className="">
       <Hero />
-      <div className="w-full min-h-screen">
+      <div className="w-full">
         <Workflow />
       </div>
+      <div className="w-full min-h-screen">
+        <Specs />
+      </div>
+      <div className="w-full min-h-screen">
+        <MobileAppSection />
+      </div>
+      <div className="w-full min-h-screen"></div>
       <div className="w-full min-h-screen"></div>
     </div>
   );
