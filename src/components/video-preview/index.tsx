14fcfc5b326d707e063bc7a081c53caf291b25f0
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-06 22:53:45 +0600

    Add VideoPreview component for displaying YouTube videos

---
+++ src/components/video-preview/index.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import YouTube, { YouTubeProps } from 'react-youtube';
+import { useRef, useState, useEffect } from 'react';
+
+export default function VideoPreview() {
+  const [width, setWidth] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const opts: YouTubeProps['opts'] = {
+    height: (Math.floor(width * 0.95 * 0.5625)).toString(),
+    width: (Math.floor(width * 0.95)).toString(),
+  };
+
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setWidth(containerRef.current.clientWidth);
+    }
+
+    const handleResize = () => {
+      if (containerRef.current) {
+        setWidth(containerRef.current.clientWidth);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
+
+  return (
+    <div
+    ref={containerRef}
+      className="relative w-full aspect-video max-w-5xl mx-auto p-5 border border-dashed flex justify-center items-center"
+    >
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+      <h1 className='head-text-md'>Video coming soon</h1>
+       {/* <YouTube videoId="eR66m7TaV5A" opts={opts} /> */}
+    </div>
+  );
+}
+
+const Icon = ({ className, ...rest }: any) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={className}
+      {...rest}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+    </svg>
+  );
+};
