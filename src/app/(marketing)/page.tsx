7e2a708a286219049a980f79c533f8a218206553
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-06 22:53:11 +0600

    Update (marketing) page component with dynamic imports and video preview

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,14 +1,20 @@
 "use client";
-
 import Hero from "@/components/hero";
-import MobileAppSection from "@/components/mobile-app";
-import Specs from "@/components/specifications";
-import Workflow from "@/components/workflow";
+import VideoPreview from "@/components/video-preview";
+import dynamic from "next/dynamic";
+
+const MobileAppSection = dynamic(() => import("@/components/mobile-app"));
+const Products = dynamic(() => import("@/components/products"));
+const Specs = dynamic(() => import("@/components/specifications"));
+const Workflow = dynamic(() => import("@/components/workflow"));
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <div className="w-full mt-24 lg:mt-0 lg:min-h-screen flex justify-center items-center">
+        <VideoPreview />
+      </div>
       <div className="w-full">
         <Workflow />
       </div>
@@ -18,8 +24,9 @@ export default function Home() {
       <div className="w-full min-h-screen">
         <MobileAppSection />
       </div>
-      <div className="w-full min-h-screen"></div>
-      <div className="w-full min-h-screen"></div>
+      <div className="w-full min-h-screen">
+        <Products />
+      </div>
     </div>
   );
 }
