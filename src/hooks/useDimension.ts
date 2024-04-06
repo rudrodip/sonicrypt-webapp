47d02d3444fed4c3637dd69ba4bf59ff8fdc04ab
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-06 22:53:55 +0600

    Fix window object error in useDimension hook

--- src/hooks/useDimension.ts
+++ src/hooks/useDimension.ts
@@ -1,23 +1,24 @@
 import { useState, useEffect } from "react";
 
+
 export const useDimension = () => {
+  const isClient = typeof window !== "undefined";
   const [dimension, setDimension] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: isClient ? window.innerWidth : 0,
+    height: isClient ? window.innerHeight : 0,
   });
 
   useEffect(() => {
+    if (!isClient) return;
+
     const listener = () => {
-      setDimension({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
+      setDimension({ width: window.innerWidth, height: window.innerHeight });
     };
 
     window.addEventListener("resize", listener);
 
     return () => window.removeEventListener("resize", listener);
-  }, []);
+  }, [isClient]);
 
   return dimension;
 };
