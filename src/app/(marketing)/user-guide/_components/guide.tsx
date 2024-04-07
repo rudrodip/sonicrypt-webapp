15cd5424746b1d68657266b4a52647549d357429
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-07 19:59:39 +0600

    Update GuideSection component to use VideoJs for media playback

--- src/app/(marketing)/user-guide/_components/guide.tsx
+++ src/app/(marketing)/user-guide/_components/guide.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Steps from "./steps";
+import { VideoJs } from "@/components/video-component";
 
 type StepsProps = {
   title: string;
@@ -10,7 +10,7 @@ type StepsProps = {
 export default function GuideSection({ title, steps, media }: StepsProps) {
   return (
     <section className="w-full max-w-6xl mx-auto my-6 md:my-12 lg:my-20">
-      <div className="w-full flex flex-wrap md:flex-nowrap justify-between">
+      <div className="w-full flex flex-wrap md:flex-nowrap justify-center md:justify-between">
         <div className="flex flex-col gap-4 md:gap-8 lg:gap-12">
           <h1 className="text-6xl font-semibold tracking-tight">{title}</h1>
           <div
@@ -21,13 +21,24 @@ export default function GuideSection({ title, steps, media }: StepsProps) {
           </div>
         </div>
         <div className="flex justify-center md:justify-end items-center mt-5 md:mt-0">
-          <Image
-            src={media}
-            alt={title}
-            width={400}
-            height={600}
-            className="w-[66%] md:w-1/2"
-          />
+          <div className="relative w-[300px] aspect-[1/2] flex flex-col border-4 rounded-lg justify-between items-center bg-zinc-900 mx-auto">
+            <div
+              id="punch-hole-camera"
+              className="absolute w-3 h-3 rounded-full bg-zinc-800 border border-zinc-700 left-3 top-3 z-10"
+            />
+            <VideoJs
+              options={{
+                muted: true,
+                sources: [
+                  {
+                    src: media,
+                    type: "video/mp4",
+                  },
+                ],
+              }}
+              fallbackImageUrl="/logo.png"
+            />
+          </div>
         </div>
       </div>
     </section>
