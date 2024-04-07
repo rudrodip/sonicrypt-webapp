d7aafbec670c017ee54427b8b6d98b7193775169
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-07 12:14:37 +0600

    Add user guide page component

---
+++ src/app/(marketing)/user-guide/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="w-full max-w-7xl mx-auto">
+      <h1 className="head-text text-center">User Guide</h1>
+      <p>This is the user guide page.</p>
+    </main>
+  );
+}
