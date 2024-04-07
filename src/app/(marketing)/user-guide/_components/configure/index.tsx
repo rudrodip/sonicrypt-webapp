52e1035aea1433c037d3f9e8c5bd2dec5c28d895
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-07 13:02:08 +0600

    changed

--- src/app/(marketing)/user-guide/_components/configure/index.tsx
+++ src/app/(marketing)/user-guide/_components/configure/index.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function ConfigureSection() {
     return (
         <>
-            <h1 className="head-text-md">Connect</h1>
+            <h1 className="head-text-md">Configure</h1>
             <p>Let&apos;s connect your device to the app</p>
         </>
     )
