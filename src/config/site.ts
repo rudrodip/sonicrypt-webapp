6b87b8fd57bb554c90bab2c50cadf235b981ccfc
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-19 21:59:11 +0600

    updated site url

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig: SiteConfig = {
   name: "Sonicrypt",
   description:
     "Sonicrypt is a compact and intuitive device designed to enhance the user experience for both buyers and sellers engaging in crypto transactions",
-  url: "https://sonicrypt.vercel.app",
-  ogImage: "https://sonicrypt.vercel.app/og.png",
+  url: "https://sonicrypt.rdsx.dev",
+  ogImage: "https://sonicrypt.rdsx.dev/og.png",
   links: {
     twitter: "https://twitter.com/Sonicrypt",
     github: "https://github.com/Sonicrypt",
