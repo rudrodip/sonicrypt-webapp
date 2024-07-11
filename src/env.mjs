54a2626ad231a37e2a73379228a493ec6149831b
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-11 20:09:32 +0600

    added clerk api keys

--- src/env.mjs
+++ src/env.mjs
@@ -4,10 +4,18 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
+    GEMINI_API_KEY: z.string().min(1),
+    AUTH_WEBHOOK_SECRET: z.string().min(1),
   },
   client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    AUTH_WEBHOOK_SECRET: process.env.AUTH_WEBHOOK_SECRET,
   },
 });
