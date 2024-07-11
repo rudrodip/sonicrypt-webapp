c148b218cf3973bc1a398bae3e78c0ba3e2e5ace
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-11 20:10:11 +0600

    init sign up

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import * as Clerk from '@clerk/elements/common';
+import * as SignUp from '@clerk/elements/sign-up';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Icons } from '@/components/icons';
+
+export default function SignUpPage() {
+  return (
+    <div className="grid w-full grow items-center px-4 sm:justify-center">
+      <SignUp.Root>
+        <Clerk.Loading>
+          {(isGlobalLoading) => (
+            <>
+              <SignUp.Step name="start">
+                <Card className="w-full sm:w-96">
+                  <CardHeader>
+                    <CardTitle>Create your account</CardTitle>
+                    <CardDescription>Welcome! Please fill in the details to get started.</CardDescription>
+                  </CardHeader>
+                  <CardContent className="grid gap-y-4">
+                    <div className="grid grid-cols-2 gap-x-4">
+                      <Clerk.Connection name="github" asChild>
+                        <Button size="sm" variant="outline" disabled={isGlobalLoading}>
+                          <Clerk.Loading scope="provider:github">
+                            {(isLoading) =>
+                              isLoading ? (
+                                <Icons.spinner className="size-4 animate-spin" />
+                              ) : (
+                                <>
+                                  <Icons.gitHub className="mr-2 size-4" />
+                                  GitHub
+                                </>
+                              )
+                            }
+                          </Clerk.Loading>
+                        </Button>
+                      </Clerk.Connection>
+                      <Clerk.Connection name="google" asChild>
+                        <Button size="sm" variant="outline" disabled={isGlobalLoading}>
+                          <Clerk.Loading scope="provider:google">
+                            {(isLoading) =>
+                              isLoading ? (
+                                <Icons.spinner className="size-4 animate-spin" />
+                              ) : (
+                                <>
+                                  <Icons.google className="mr-2 size-4" />
+                                  Google
+                                </>
+                              )
+                            }
+                          </Clerk.Loading>
+                        </Button>
+                      </Clerk.Connection>
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <div className="grid w-full gap-y-4">
+                      <SignUp.Action submit asChild>
+                        <Button disabled={isGlobalLoading}>
+                          <Clerk.Loading>
+                            {(isLoading) => {
+                              return isLoading ? <Icons.spinner className="size-4 animate-spin" /> : 'Continue';
+                            }}
+                          </Clerk.Loading>
+                        </Button>
+                      </SignUp.Action>
+                      <Button variant="link" size="sm" asChild>
+                        <Link href="/sign-in">Already have an account? Sign in</Link>
+                      </Button>
+                    </div>
+                  </CardFooter>
+                </Card>
+              </SignUp.Step>
+            </>
+          )}
+        </Clerk.Loading>
+      </SignUp.Root>
+    </div>
+  );
+}
