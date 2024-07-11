e168aeaed2fa494faf07c63d7e1485fb943fdbe1
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-11 21:46:15 +0600

    todo

--- src/components/video-component.tsx
+++ src/components/video-component.tsx
@@ -22,17 +22,17 @@ export const VideoJs = ({ options, fallbackImageUrl }: IVideoPlayerProps) => {
   const videoNode = React.useRef<HTMLVideoElement | null>(null);
   const player = React.useRef<videojs.Player>();
 
-  React.useEffect(() => {
-    player.current = videojs(videoNode.current, {
-      ...initialOptions,
-      ...options,
-    }).ready(function () {});
-    return () => {
-      if (player.current) {
-        player.current.dispose();
-      }
-    };
-  }, [options]);
+  // React.useEffect(() => {
+  //   player.current = videojs(videoNode.current, {
+  //     ...initialOptions,
+  //     ...options,
+  //   }).ready(function () {});
+  //   return () => {
+  //     if (player.current) {
+  //       player.current.dispose();
+  //     }
+  //   };
+  // }, [options]);
 
   return (
     <div className="w-full aspect-[1/2.23] flex justify-center items-center rounded-lg overflow-hidden">
