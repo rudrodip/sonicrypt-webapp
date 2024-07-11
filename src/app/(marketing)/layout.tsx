f88c6176ef40f1a281799d2b655abbdc0eef77b7
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-11 20:11:38 +0600

    added bg layer

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./_components/navbar";
 import Footer from "@/components/footer";
+import BG from "@/components/bg";
 
 export default function RootLayout({
   children,
@@ -8,10 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <section className="flex min-h-screen flex-col">
+      <BG />
       <Navbar />
-      <main className="flex-1 w-full px-2 md:px-0">
-        {children}
-      </main>
+      <main className="flex-1 w-full px-2 md:px-0">{children}</main>
       <Footer />
     </section>
   );
